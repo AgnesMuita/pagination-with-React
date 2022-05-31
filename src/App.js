863158d13ed85cce5030e232d10f5303564ec213
Agnes Muita <agnes.muita@alternateltd.com>
2022-05-31 23:16:14 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+import React,{useState} from "react";
+import ReactPaginate from 'react-paginate';
 import './App.css';
 
-function App() {
+
+const NewsCard =(props)=>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding:'20'}}>
+      <a href = {props.url}>
+        {props.title} by  {props.author}
+      </a>
     </div>
-  );
+  )
+
+}
+function App() {
+    
+  const[hitss, setHits]=useState([]);
+  const[dataisLoading, setDataIsLoading] = useState(false);
+  const[query,setQuery] = useState('startups');
+  const[pageCount, setPageCount] = useState(1);
+  const[currentPage, setCurrentPage] = useState(0);
+
+  const handleChange=()=>{
+    fetch(`https://hn.algolia.com/api/v1/search?query=${query}`)
+    .then(res=>res.json())
+    .then(body=>{
+        console.log(body)
+        setHits([...body.hits])
+        setPageCount(body.nbPages);
+        setDataIsLoading(true)
+    })
+    .catch(error=>console.error("ERROR",error))
+  }
+
+  const handleInput=(event)=>{
+    setQuery(event.target.value)
+  }
+
+  const handlePageChange=(selectedObject)=>{
+      setCurrentPage(selectedObject.selected);
+      handleChange();
+  }
+
+  return (
+      <div style={{paddingTop:'200'}}>
+        <input type="text" onChange={handleInput}/>
+        <button onClick={handleChange}>Get Data</button>
+        {dataisLoading?( 
+          hitss.map((item)=>{ 
+            // console.log(item)
+              return(             
+                <NewsCard 
+                  comment_text={item.comment_text}
+                  story_text={item.story_text}
+                  story_url={item.story_url}
+                  title={item.title}
+                  author={item.author}
+                  key={item.objectID}>
+                </NewsCard>
+              )           
+          })
+        ):(
+        <div></div>
+        )}
+        {dataisLoading?(
+          <ReactPaginate
+              pageCount={pageCount}
+              pageRange={2}
+              marginPagesDisplayed={2}
+              onPageChange={handlePageChange}
+              containerClassName={'container'}
+              previousLinkClassName={'page'}
+              breakClassName={'page'}
+              nextLinkClassName={'page'}
+              pageClassName={'page'}
+              disabledClassNae={'disabled'}
+              activeClassName={'active'}
+          />
+        ):(
+            <div>Nothing to display</div>
+        )}
+      </div>
+  )
 }
 
 export default App;
